d4907b7c7d523be6743ae9f029fcfa18278e75f7
biamot4 <beatrizmotasesi@gmail.com>
2024-11-12 09:16:58 -0300

    Add files via upload

---
+++ ENIGMA DO RIVOTRIL.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	int op, resp = 4;
+	
+	do{
+	setlocale(LC_ALL, "portuguese");
+	printf("\nENIGMA DO RIVOTRIL:\n");
+	printf("Chave profunda e desconhecida:\n2 15 18 1   17 21 5 18 5 18?\n");
+	printf("\nResolva para descobrir:\n2x + 5 = 15\nx = ");
+	scanf("%d", &op);
+	if(op == 5){
+		printf("Revise o código e encontre o erro!\n");
+	} else if(op == resp){
+		printf("\nFrase descriptografada:\nBora querer?");
+	}
+} while (op != 4);
+}
